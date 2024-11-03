70916c8b98f7cd6a78aa480c9911742e0597e56e
gtgopinath5 <gtgopinath5@gmail.com>
2024-11-03 20:26:53 +0530

    first commit

---
+++ helpers/cloudinary.js
@@ -0,0 +1,22 @@
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY, // Add this
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Add this
+});
+
+const storage = new multer.memoryStorage();
+
+async function imageUploadUtil(file) {
+  const result = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+
+  return result;
+}
+
+const upload = multer({ storage });
+
+module.exports = { upload, imageUploadUtil };
